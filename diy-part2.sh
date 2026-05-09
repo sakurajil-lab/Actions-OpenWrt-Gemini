9367e2b5e7d34f82ab253b2c9040dd202442abc8
sakurajil-lab <sakura13106980881@gmail.com>
2026-05-09 13:59:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,8 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# 1. 修改默认 IP 为 192.168.5.2
+sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 2. 修改默认网关为 192.168.5.1
+sed -i '/set network.$1.ipaddr/a \t\tset network.$1.gateway="192.168.5.1"\n\t\tset network.$1.dns="192.168.5.1"' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 移除冗余的无线配置（针对你不需要 wifi 的需求，虽然不能物理拆除，但能精简软件层）
+# 这一步通常在 menuconfig 处理，脚本保留默认即可
